157c8e8035ccd1505e7ec4193198de5e4d5f1858
Eden <eden@gigaspaces.com>
2015-06-08 19:16:34 +0300

    feat(Jshint Added a jshint task added a jshint task to grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,5 +2,13 @@
 module.exports = function(grunt) {
     grunt.registerTask('test', function () {
         console.log("My task is running !");
-    })
+    });
+    grunt.initConfig({
+        jshint: {
+            all: ['Gruntfile.js', './*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
 };
